21c3d02f1fcb204ff84ee5016ef1c95dc5e15b10
Antwan Gaggi <antwankakki@gmail.com>
2016-02-27 20:33:05 -0800

    Lets go boys

---
+++ src/mechanics/BeeCell.java
@@ -0,0 +1,36 @@
+package mechanics;
+
+/**
+ * This class represents a bee cell. It gives references to the neighbors of the cell and 
+ * allows the user to check on the kids.
+ * @author antwan
+ *
+ */
+public class BeeCell {
+	public static final int TOP_LEFT = 0, TOP = 1, TOP_RIGHT = 2, BOTTOM_RIGHT = 3,
+							BOTTOM = 4, BOTTOM_LEFT = 5;
+	
+	public static final int PESTICIDE = 6, DYING = 5, VERY_SICK = 4, SICK = 3, 
+							NORMAL = 2, VERY_HAPPY = 1; 
+	
+	/* The honeycomb's neighbors */
+	public BeeCell[] neighbors = new BeeCell[6];
+	
+	/* Status of the bee */
+	private int mStatus = 0;
+	
+	/**
+	 * Constructor
+	 */
+	public BeeCell(){}
+	
+	/**
+	 * Returns the status of the bee responsible for this hive
+	 * @return
+	 */
+	public int getStatus()
+	{
+		return mStatus;
+	}
+	
+}
