ef6f54b1829503fc7941ace06b868b1688d756a4
Fadee Kannah <fadeekannah@gmail.com>
2016-02-28 01:51:59 -0800

    Added test api that dumps data for the us, and login now is semi functional

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,16 +4,21 @@ module.exports = function(){
     console.log(req.session);
     res.json({success:true});
   })
-  router.get('/login',function(req,res){
-    req.session.nickName = 'fadee';
-    res.json({login:true});
+  router.post('/login',function(req,res){
+    if(req.body.hasOwnProperty('nickName')){
+      req.session.nickName = req.body.nickName;
+      res.json({login:true});
+    }
+    else{
+      res.json({login:false});
+    }
   })
   router.get('/logout',function(req,res){
     req.session.destroy();
     res.json({logout:true});
   })
   router.get('/test',function(req,res){
-    res.json(req.sessionID);
+    res.json({body:req.body,query:req.query,params:req.params,sessionID:req.sessionID,session:req.session});
   })
   return router;
 }
